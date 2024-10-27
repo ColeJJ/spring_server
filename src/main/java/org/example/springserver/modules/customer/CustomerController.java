8e0f37bad0a00587171f681e4f2ffb4517614c29
ColeJJ <torben.unland@gmail.com>
2024-10-27 18:51:53 +0100

    changed exception in endpoint "/customer" and "/customer/{id}"

--- src/main/java/org/example/springserver/modules/customer/CustomerController.java
+++ src/main/java/org/example/springserver/modules/customer/CustomerController.java
@@ -2,10 +2,16 @@ package org.example.springserver.modules.customer;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("/api")
 public class CustomerController {
 
   final CustomerRepository customerRepository;
@@ -14,8 +20,18 @@ public class CustomerController {
     this.customerRepository = customerRepository;
   }
 
-  @GetMapping("/api/customers")
+  @GetMapping("/customers")
   public List<Customer> getCustomers() {
     return customerRepository.findAll();
+  }
+
+  @GetMapping("/customer/{id}")
+  public Customer getCustomerByPathId(@PathVariable Integer id) {
+    return customerRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+  }
+
+  @GetMapping("/customer")
+  public Customer getCustomerByParamID(@RequestParam Integer id) {
+    return customerRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
 }
